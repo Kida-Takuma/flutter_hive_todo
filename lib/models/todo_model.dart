df49acc159f8b68339846333343cda7ee9540554
TakumaKida <kida.takuma100@gmail.com>
2022-09-19 17:17:04 +0900

    Create TodoModel

---
+++ lib/models/todo_model.dart
@@ -0,0 +1,5 @@
+class TodoModel {
+  TodoModel({required this.title, required this.complete});
+  String title;
+  bool complete;
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -30,6 +30,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
+      body: ListView.builder(),
     );
   }
 }
