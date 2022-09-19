2c771bd10dfa9c403b86a9863d7763d4516733a5
TakumaKida <kida.takuma100@gmail.com>
2022-09-19 17:13:36 +0900

    Add AppBar

---
+++ lib/pages/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text(
+          "HiveTodoApp",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.add,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
